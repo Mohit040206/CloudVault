8a10e5b3fd6428090a4e2b8b7aec012c4801660b
Mohit040206 <mohitkumarpath@gmail.com>
2025-08-15 13:22:57 +0530

    Initial commit - CloudVault project setup

---
+++ src/main/java/com/cloudvault/model/Admin.java
@@ -0,0 +1,41 @@
+package com.cloudvault.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name= "admins")
+public class Admin {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+    @Column(nullable = false)
+    private String password;
+    public Admin(){}
+    public Admin(String email, String password){
+        this.email=email;
+        this.password=password;
+    }
+    public Long getId(){
+        return id;
+    }
+
+
+
+    public String getEmail() {
+        return email;
+    }
+    public void setEmail(String email) {
+        this.email = email;
+    }
+    public void setPassword(String password){
+        this.password=password;
+    }
+    public String getPassword(){
+        return password;
+    }
+
+}
---
+++ src/main/java/com/cloudvault/model/User.java
@@ -0,0 +1,67 @@
+package com.cloudvault.model;
+
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String phoneNo;
+
+    @Column(nullable = false,unique = true)
+    private String studentId;
+
+    @Column(nullable = false,unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String name;
+
+    public String getStudentId() {
+        return studentId;
+    }
+
+    public void setStudentId(String studentId) {
+        this.studentId = studentId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getPhoneNo() {
+        return phoneNo;
+    }
+
+    public void setPhoneNo(String phoneNo) {
+        this.phoneNo = phoneNo;
+    }
+
+    public void setEmail(String email){
+        this.email=email;
+    }
+
+    public String getEmail(){
+        return email;
+    }
+
+    public void setPassword(String password){
+        this.password=password;
+    }
+
+    public String getPassword(){
+        return password;
+    }
+}
